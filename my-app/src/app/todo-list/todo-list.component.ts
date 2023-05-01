e8a74b6f12ffb40fd9210afb9da3e593183fa2c8
amarinder1029 <109933668+amarinder1029@users.noreply.github.com>
2023-04-30 20:52:13 -0700

    basic todo-list, basic styling

--- my-app/src/app/todo-list/todo-list.component.ts
+++ my-app/src/app/todo-list/todo-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Todo {
+  title: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -10,6 +15,28 @@ export class TodoListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  todos: Todo[] = [
+    { title: 'Learn Angular', completed: false },
+    { title: 'Build a to-do list', completed: false },
+    { title: 'Deploy the app', completed: false }
+  ];
+  newTodo: string = '';
+
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push({ title: this.newTodo, completed: false });
+      this.newTodo = '';
+    }
+  }
+
+  toggleCompleted(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  deleteTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
   }
 
 }
