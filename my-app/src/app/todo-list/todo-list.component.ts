3c755d9b039f92f33a0372a4a312f06e11e96c14
amarinder1029 <109933668+amarinder1029@users.noreply.github.com>
2023-05-11 18:05:51 -0700

    searchtask works

--- my-app/src/app/todo-list/todo-list.component.ts
+++ my-app/src/app/todo-list/todo-list.component.ts
@@ -12,15 +12,18 @@ export class TodoListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+    this.filteredTodos=this.todos;
 
   }
+  filteredTodos: Todo[] = [];
   todos: Todo[] = [
     { title: 'Learn Angular', completed: false },
     { title: 'Build a to-do list', completed: false },
     { title: 'Deploy the app', completed: false }
   ];
   newTodo: string = '';
-
+  searchItem: string = '';
   addTodo() {
     if (this.newTodo.trim() !== '') {
       this.todos.push({ title: this.newTodo, completed: false });
@@ -35,6 +38,21 @@ export class TodoListComponent implements OnInit {
   deleteTodo(todo: Todo) {
     const index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);
+    this.search();
   }
+  search() {
+    // Reset the filteredTodos array
+    this.filteredTodos = [];
 
+    // If there is a search term
+    if (this.searchItem.trim() !== '') {
+      const searchTerm = this.searchItem.toLowerCase().trim();
+      this.filteredTodos = this.todos.filter(todo =>
+        todo.title.toLowerCase().includes(searchTerm)
+      );
+    } else {
+      // If there is no search term, show all todos
+      this.filteredTodos = this.todos;
+    }
+  }
 }
