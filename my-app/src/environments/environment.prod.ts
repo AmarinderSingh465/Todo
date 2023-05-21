3b2cdbcec00d25473e91f8591613f2c2ffb2c06f
amarinder1029 <109933668+amarinder1029@users.noreply.github.com>
2023-05-21 13:33:40 -0700

    deployed app

--- my-app/src/environments/environment.prod.ts
+++ my-app/src/environments/environment.prod.ts
@@ -1,3 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: true
+  production: true,
+    firebase : {
+      apiKey: "AIzaSyDnnhgi-9c0A8ztxS__soaVhgr2bIrI-iA",
+      authDomain: "todo-list-8ca30.firebaseapp.com",
+      databaseURL: "https://todo-list-8ca30-default-rtdb.firebaseio.com",
+      projectId: "todo-list-8ca30",
+      storageBucket: "todo-list-8ca30.appspot.com",
+      messagingSenderId: "446882680981",
+      appId: "1:446882680981:web:3f6a7a550c632d7427cb9c",
+      measurementId: "G-99Q32D8P93"
+  }
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -3,15 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
     firebase : {
-    apiKey: "AIzaSyDnnhgi-9c0A8ztxS__soaVhgr2bIrI-iA",
-    authDomain: "todo-list-8ca30.firebaseapp.com",
-    projectId: "todo-list-8ca30",
-    storageBucket: "todo-list-8ca30.appspot.com",
-    messagingSenderId: "446882680981",
-    appId: "1:446882680981:web:b32a5af98cd7b97a27cb9c",
-    measurementId: "G-GCR7RT2RWC"
+      apiKey: "AIzaSyDnnhgi-9c0A8ztxS__soaVhgr2bIrI-iA",
+      authDomain: "todo-list-8ca30.firebaseapp.com",
+      databaseURL: "https://todo-list-8ca30-default-rtdb.firebaseio.com",
+      projectId: "todo-list-8ca30",
+      storageBucket: "todo-list-8ca30.appspot.com",
+      messagingSenderId: "446882680981",
+      appId: "1:446882680981:web:3f6a7a550c632d7427cb9c",
+      measurementId: "G-99Q32D8P93"
   }
 };
 
